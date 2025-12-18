c72749c98e3bcae2c712a7cac32446bf9d7caaf8
Donnchadh <dgushchenko228@gmail.com>
2025-12-18 22:56:32 +0300

    feat(server): add forbidden error

--- server/middleware/validationMiddleware.js
+++ server/middleware/validationMiddleware.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const ApiError = require("../expectations/apiError");
+
 module.exports = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
--- server/tests/groupService.test.js
+++ server/tests/groupService.test.js
@@ -12,7 +12,11 @@ describe("Group test", () => {
   const modelGroup = { id: 1, name: "Group", userId: 42, createAt: new Date(), updateAt: new Date() };
 
   beforeEach(() => {
-    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
   });
 
   it("Must create a group and return its data from the DB", async () => {
@@ -36,6 +40,7 @@ describe("Group test", () => {
     const newName = "New name2";
     const mockGroup = { ...modelGroup };
 
+    //mock that emulates the mutation of an object and its storage in the database
     const mockSave = jest.fn().mockImplementation(() => {
       mockGroup.name = newName;
       return mockGroup;
@@ -55,11 +60,12 @@ describe("Group test", () => {
   });
   it("must delete group", async () => {
     const id = 1;
+
     Group.destroy.mockResolvedValue(1);
 
     const result = await groupService.removeGroup(id);
-    expect(Group.destroy).toHaveBeenCalledTimes(1);
 
+    expect(Group.destroy).toHaveBeenCalledTimes(1);
     expect(Group.destroy).toHaveBeenCalledWith({ where: { id: 1 } });
     expect(result).toBe(1);
   });
@@ -75,7 +81,8 @@ describe("Group test", () => {
       { ...modelGroup, id: 4, name: "group4" },
     ];
 
-    Group.findAndCountAll.mockResolvedValue({ count: allGroup.length, rows: allGroup.slice(offset, offset + limit) });
+    const params = { count: allGroup.length, rows: allGroup.slice(offset, offset + limit) };
+    Group.findAndCountAll.mockResolvedValue(params);
 
     const result = await groupService.getAllGroup(limit, offset, userId);
 
