6b3e9016da48d6356256372ee26fc6236b0072b9
Donnchadh <dgushchenko228@gmail.com>
2025-12-24 23:03:15 +0300

    refactor(server): improve client-side integration and CORS
    - Renamed the `UnauthorizedError` method (within the `ApiError` class) to `Unauthorized`.
    - Added an endpoint to retrieve user data required for client-side authorization checks.
    - Added the `tokenId` parameter to `router.delete('/logout')` and modified CORS settings.

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,19 +5,20 @@ module.exports = (req, res, next) => {
   try {
     const authorizationHeader = req.headers.authorization;
     if (!authorizationHeader) {
-      return next(ApiError.UnauthorizedError());
+      return next(ApiError.Unauthorized());
     }
     const accessToken = authorizationHeader.split(" ")[1];
     if (!accessToken) {
-      return next(ApiError.UnauthorizedError());
+      return next(ApiError.Unauthorized());
     }
     const userData = tokenService.validateAccessToken(accessToken);
     if (!userData) {
-      return next(ApiError.UnauthorizedError());
+      return next(ApiError.Unauthorized());
     }
+
     req.user = userData;
     next();
   } catch (error) {
-    return next(ApiError.UnauthorizedError());
+    return next(ApiError.Unauthorized());
   }
 };
